abea531071bb9e17eb67694504e1e273b98f0af3
ELkhlouf <aafrae96@gmail.com>
2023-04-25 15:45:54 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+
+/**
+ * main - a program that prints _putchar
+ * not using any of the standard library functions
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
