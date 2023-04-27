feaabddbf0b7d0ada8ae8178175719601c120eee
ELkhlouf <aafrae96@gmail.com>
2023-04-26 17:09:02 -0700

    final commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,3 +7,17 @@
  */
 int main(void)
 {
+	int a = 1, b = 2, c, count = 0;
+
+	printf("%d, %d, ", a, b);
+	while (count < 96)
+	{
+		c = a + b;
+		printf("%d, ", c);
+		a = b;
+		b = c;
+		count++;
+	}
+	printf("%d\n", a + b);
+	return (0);
+}
